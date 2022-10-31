86d27144059c9c64c1e40a80857d9a0872e994db
NelmiEK99 <nelmikudagodage@gmail.com>
2022-10-31 19:24:31 +0530

    result page added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
+import ResultPage from "./Pages/ResultPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route exact path="*" element={<Home />} />
+          <Route exact path="/resultpage" element={<ResultPage />} />
         </Routes>
       </BrowserRouter>
     </div>
